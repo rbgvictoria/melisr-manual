18fc3e84000327d7728d580a0327224264606aa1
Niels Klazenga <NielsKlazenga@rbg.vic.gov.au>
2017-11-03 11:34:08 +1100

    Rather big commitment

--- assets/js/jquery.melisr-manual.js
+++ assets/js/jquery.melisr-manual.js
@@ -1,19 +1,20 @@
 $(function() {
-    if ($('#toc').length > 0 && $('#toc').children('.nav').length > 0) {
-        $('#toc>.nav:eq(0)').affix({
-            offset: {
-                top: $('#toc>.nav:eq(0)').offset().top,
-                bottom: $('footer').height() + 40
-            }
-        });
-
-        $('body').scrollspy({
-            target: "#toc",
-            offset: 30
-        });
-    }
-    
     if ($(window).width() > 992) {
+
+        if ($('#toc').length > 0 && $('#toc').children('.nav').length > 0) {
+            $('#toc>.nav:eq(0)').affix({
+                offset: {
+                    top: $('#toc>.nav:eq(0)').offset().top,
+                    bottom: $('footer').height() + 40
+                }
+            });
+
+            $('body').scrollspy({
+                target: "#toc",
+                offset: 30
+            });
+        }
+    
         var navbar = $('#navbar>ul:eq(0)');
         
         var dropdown = $('<li/>', {
